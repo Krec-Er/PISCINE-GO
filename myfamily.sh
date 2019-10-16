14bbbd2bead6b5f7cb3437f5771b1e7019a57bc3
Krec-Er <asport@mail.kz>
2019-10-16 20:02:19 +0600

    myfamily

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-$ curl -s 'https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json' | jq ' .[] | select( .id == 1 )' | grep 'relatives' | cut -d '"' -f4
+curl -s 'https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json' | jq ' .[] | select( .id == 1 )' | grep 'relatives' | cut -d '"' -f4
