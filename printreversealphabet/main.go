1df9281edfe9c4c04f835a2521030ff6537f35d2
Krec-Er <asport@mail.kz>
2019-10-17 23:27:05 +0600

    reversealphabet

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -2,12 +2,13 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main() {
+func main () {
 
-	for i := 'z'; i >= 'a'; i-- {
+	for i := 'z'; i >= 'a' ; i-- {
 
 		z01.PrintRune(i)
 
-	} 
+	}
 	z01.PrintRune(10)
+
 }
