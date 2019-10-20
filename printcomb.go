f6ae4731ff6be15e70084e8c690df85d1f80ddc1
Krec-Er <asport@mail.kz>
2019-10-20 16:03:59 +0600

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -3,9 +3,9 @@ package piscine
 import "github.com/01-edu/z01"
 
 func printcomb() {
-	for i:= '0'; i<='9'; i++ {
-		for j:= '0'; j<= '9'; j++ {
-			for:='0'; k<= '9'; k++ {
+	for i := '0'; i<='9'; i++ {
+		for j := '0'; j<= '9'; j++ {
+			for k :='0'; k<= '9'; k++ {
 				if i<j && j<k {
 				z01.PrintRune(i)
 				z01.PrintRune(j)
