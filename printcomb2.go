d79a1073cc4b3639173191c5ae18680304fd6134
Krec-Er <asport@mail.kz>
2019-10-20 16:41:08 +0600

    printcomb2

---
+++ printcomb2.go
@@ -0,0 +1,28 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				for n := '0'; n <= '9': n++ {
+					if i != k && j != n {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(n)
+
+
+						if i != '9' || j != '8' || k != '9' || n!= '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune(10)
+}
