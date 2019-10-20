3fb854e0e0b815798a9ff0f80212bf6bf68412d5
Krec-Er <asport@mail.kz>
2019-10-20 17:47:27 +0600

    printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -7,14 +7,14 @@ func PrintComb2() {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for n := '0'; n <= '9'; n++ {
-					if i != k || j != n {
+					if i != k && j != n {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(' ')
 						z01.PrintRune(k)
 						z01.PrintRune(n)
 
-						if i == '9' || j == '8' || k == '9' || n == '9' {
+						if i == '9' && j == '8' && k == '9' && n == '9' {
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						}
