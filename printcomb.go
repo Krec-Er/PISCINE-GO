0101e35ec41b6206fb3d9df414ec6156b3ded465
Krec-Er <asport@mail.kz>
2019-10-20 16:26:34 +0600

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -11,7 +11,7 @@ func PrintComb() {
 					z01.PrintRune(j)
 					z01.PrintRune(k)
 
-					if i == '7' && j == '8' && k == '9' {
+					if i != '7' && j != '8' && k != '9' {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
