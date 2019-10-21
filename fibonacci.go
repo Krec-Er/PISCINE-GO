84028d2aef030166ace435409ed6bd049e437196
Krec-Er <asport@mail.kz>
2019-10-21 15:23:14 +0600

    Fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,14 @@
+package piscine
+
+func fibonacci(nb int) int {
+
+	if nb < 0 {
+		return -1
+	} else if nb == 0 {
+		return 0
+	} else if nb == 1 {
+		return 1
+	} else {
+		return fibonacci(nb-1) + fibonacci(nb-2)
+	}
+}
