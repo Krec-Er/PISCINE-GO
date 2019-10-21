6d358ba0309df4789930752eb9e5746fb13f3233
Krec-Er <asport@mail.kz>
2019-10-21 13:46:46 +0600

    Iterative Factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,17 +1,17 @@
 package piscine
 
-func IterativeFactorial (nb int) int {
-		
-		if nb < 0 {
-			return 0
-		} else if nb == 0 || nb == 1 {
-			return 1
-		}
-		
-		result := 1
+func IterativeFactorial(nb int) int {
 
-		for i:= 1; i <= nb; i++ {
-			result = result * i
-		}
-	return result
+	if nb < 0 {
+		return 0
+	} else if nb == 0 || nb == 1 {
+		return 1
 	}
+
+	result := 1
+
+	for i := 1; i <= nb; i++ {
+		result = result * i
+	}
+	return result
+}
