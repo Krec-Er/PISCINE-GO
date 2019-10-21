c1f35e948c38da4e4bf256a6a6e7488d37143fef
Krec-Er <asport@mail.kz>
2019-10-21 15:02:24 +0600

    Recursive Power

--- recursivepower.go
+++ recursivepower.go
@@ -2,7 +2,9 @@ package piscine
 
 func RecursivePower(nb int, power int) int {
 
-	if power < 0 || nb == 0 {
+	if power == 0 && nb == 0 {
+		return 1
+	} else if power < 0 || nb == 0 {
 		return 0
 	} else if nb == 1 || power == 0 {
 		return 1
