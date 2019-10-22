2ac85f29a9453872a4e6d645c7d63e3177d01f53
Krec-Er <asport@mail.kz>
2019-10-22 18:09:17 +0600

    StrLen

--- strlen.go
+++ strlen.go
@@ -1,10 +1,10 @@
 package piscine
 
 func PrintStr(str string) int {
+	c := 0
 	a := []rune(str)
-	var len int
-	for i := range a {
-		len = i + 1
+	for p,_ := range a{
+		c =p+1
 	}
-	return len
+	return c
 }
