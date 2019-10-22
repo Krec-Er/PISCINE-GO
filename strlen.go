cbfa1adc17ca2e64cd843c0e7d30ce0f47c4760d
Krec-Er <asport@mail.kz>
2019-10-22 18:00:26 +0600

    StrLen

--- strlen.go
+++ strlen.go
@@ -1,8 +1,8 @@
 package piscine
 
-func PrintStr(str string) {
-
-	for index := range str {
+func PrintStr(str string) int {
+	len := [] run (str) {
+	for index, _ := range len {
 	}
 	return index + 1
 }
