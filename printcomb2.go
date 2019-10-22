df00845d0071bbde23385e69c862ae61091d6aa6
Krec-Er <asport@mail.kz>
2019-10-22 15:43:33 +0600

    Printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -5,8 +5,8 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			for k := '0'; k <= '9'; k++ {
-				for n := '0'; n <= '9'; n++ {
+			for k := i; k <= '9'; k++ {
+				for n := j; n <= '9'; n++ {
 					if i != k || j != n {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
