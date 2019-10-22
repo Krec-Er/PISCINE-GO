733dccb138f5faa77f4c85b3f32671feec9e75ea
Krec-Er <asport@mail.kz  git config --global user.name Krec-Ergit config --global user.email asport@mail.kz>
2019-10-22 13:32:01 +0600

    alphacount

--- alphacount.go
+++ alphacount.go
@@ -1,13 +1,12 @@
 package piscine
 
 func AlphaCount(str string) int {
-    counter := 0
+	counter := 0
 	slice := []rune(str)
-  	for _, word	:= range slice {			 
-        if word >= 65 && word <= 92 || word >= 97 && word <= 122 {
-             counter ++
-        }
-    }
-    return counter
+	for _, word := range slice {
+		if word >= 65 && word <= 92 || word >= 97 && word <= 122 {
+			counter++
+		}
+	}
+	return counter
 }
-
