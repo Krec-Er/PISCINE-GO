e4dca4f5f56d97db8cfe62ea060ed59791f5c213
Krec-Er <asport@mail.kz>
2019-10-23 14:11:12 +0600

    StrRev

---
+++ strrev.go
@@ -0,0 +1,13 @@
+package main
+
+
+func StrRev(s string) string {
+
+	runes := []rune(s)
+	var word string
+	count := StrLen(s)
+	for i := count - 1; i >= 0; i-- {
+		word = word + string(runes[i])
+	}
+	return word
+}
