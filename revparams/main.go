abd195e8bdd0470921cb2b9280fd9d4c7ede9c88
Krec-Er <asport@mail.kz>
2019-10-24 22:50:43 +0600

    Rev Params

--- revparams/main.go
+++ revparams/main.go
@@ -6,15 +6,18 @@ import (
 )
 
 func main() {
-	arguments := os.Args
-	strings := []string(arguments)
-	for index, strs := range strings {
-		if index >= 1 {
-			runes := []rune(strs)
-			for _, runess := range runes {
-				z01.PrintRune(runess)
-			}
-			z01.PrintRune(10)
+	funcName := os.Args[1:]
+	var lenStr int
+	for index := range funcName {
+		lenStr = index + 1
+	}
+	for i, j := 0, lenStr-1; i < j; i, j = i+1, j-1 {
+		funcName[i], funcName[j] = funcName[j], funcName[i]
+	}
+	for _, element := range funcName {
+		for _, char := range element {
+			z01.PrintRune(char)
 		}
+		z01.PrintRune('\n')
 	}
 }
