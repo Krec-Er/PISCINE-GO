d40025be0b6378670d67838fe2e6aa3b1c68c534
Krec-Er <asport@mail.kz>
2019-10-24 15:06:28 +0600

    Is Printable

---
+++ isprintable.go
@@ -0,0 +1,17 @@
+package piscine
+
+func IsPrintable(str string) bool {
+	if str == "" {
+		return false
+	}
+	Runes := []rune(str)
+	for _, word := range Runes {
+		if (word >= 'A' && word <= 'Z') ||
+			(word >= 'a' && word <= 'z') {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
