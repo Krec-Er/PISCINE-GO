190208fde015a9d2f43c784ff147792404a7d25a
Krec-Er <asport@mail.kz>
2019-10-24 12:09:55 +0600

    Index

---
+++ index.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func Index(s string, toFind string) int {
+	n := 0
+	j := []rune(s)
+	for p := range j {
+		n = p + 1
+	}
+	k := 0
+	l := []rune(toFind)
+	for r := range l {
+		n = r + 1
+	}
+
+	for i := 0; i <= n-k; i++ {
+		if toFind == s[i:i+k] {
+			return (i)
+		}
+	}
+}
+
+func main() {
+	fmt.Println(Index("Hello!", "l"))
+	fmt.Println(Index("Salut!", "alu"))
+	fmt.Println(Index("Ola!", "hOl"))
+}
