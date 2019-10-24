7ff96d90d19d5055d34500f2933d4df2508bcef1
Krec-Er <asport@mail.kz>
2019-10-24 14:53:45 +0600

    Is Alpha

--- isalpha.go
+++ isalpha.go
@@ -5,7 +5,7 @@ func IsAlpha(str string) bool {
 		return false
 	}
 	Runes := []rune(str)
-	for _, word := range Runes{
+	for _, word := range Runes {
 		if (word >= 'A' && word <= 'Z') ||
 			(word >= '0' && word <= '9') ||
 			(word >= 'a' && word <= 'z') {
